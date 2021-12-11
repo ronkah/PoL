0ee5e98547f34157578fc726f758f0cddf48a0b0
Ron Kahat <ron.kahat@celsius.network>
2021-12-11 23:08:16 +0200

    fix: update packagesa to address security vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,16 @@ module.exports = {
   mode: "development",
   plugins: [
     // new CopyWebpackPlugin(['index.html', "node_modules/bootstrap/dist/css/bootstrap.min.css", "styles/index.css", "styles/presented_by_qedit.svg"])
-    new CopyWebpackPlugin(['index.html', "styles/index.css"])
+    // new CopyWebpackPlugin(['index.html', "styles/index.css"]),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "index.html", to: "index.html" },
+        { from: "styles/index.css", to: "index.css" },
+      ],
+      options: {
+        concurrency: 100,
+      },
+    }),
+
   ],
 };
